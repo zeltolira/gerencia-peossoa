1454f46e096912eb717d8db5137c10aa253753da
unknown <zelto@hotmail.com>
2023-12-06 06:55:35 -0300

    API Home Test, Ok

--- src/main/java/gerenciamento/com/lira/gerenciapessoas/GerenciaPessoasApplication.java
+++ src/main/java/gerenciamento/com/lira/gerenciapessoas/GerenciaPessoasApplication.java
@@ -2,11 +2,19 @@ package gerenciamento.com.lira.gerenciapessoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
+@RequestMapping
 public class GerenciaPessoasApplication {
 
+	@GetMapping
+	public String GetHomeTest(){
+		return "Gestão de pessoas API. Test ok!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciaPessoasApplication.class, args);
 	}
