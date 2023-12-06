cbfbf9afc677777b65898007ffc36ca8b3d5cca3
unknown <zelto@hotmail.com>
2023-12-06 06:25:13 -0300

    init

---
+++ src/main/java/gerenciamento/com/lira/gerenciapessoas/GerenciaPessoasApplication.java
@@ -0,0 +1,13 @@
+package gerenciamento.com.lira.gerenciapessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaPessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerenciamento/com/lira/gerenciapessoas/GerenciaPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamento.com.lira.gerenciapessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
