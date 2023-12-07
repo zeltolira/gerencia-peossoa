424d99ac59f9382ed39485b048a56ae4fc60c869
unknown <zelto@hotmail.com>
2023-12-07 06:14:31 -0300

    implements pessoa controller

--- src/main/java/gerenciamento/com/lira/gerenciapessoas/pessoa/application/api/PessoaController.java
+++ src/main/java/gerenciamento/com/lira/gerenciapessoas/pessoa/application/api/PessoaController.java
@@ -1,2 +1,23 @@
-package gerenciamento.com.lira.gerenciapessoas.pessoa.application.api;public class PessoaController {
+package gerenciamento.com.lira.gerenciapessoas.pessoa.application.api;
+
+import gerenciamento.com.lira.gerenciapessoas.pessoa.application.api.request.PessoaRequest;
+import gerenciamento.com.lira.gerenciapessoas.pessoa.application.api.response.PessoaResponse;
+import gerenciamento.com.lira.gerenciapessoas.pessoa.application.service.PessoaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class PessoaController implements PessoaAPI {
+    private PessoaService pessoaService;
+
+    @Override
+    public PessoaResponse postPessoa(PessoaRequest pessoaRequest) {
+        log.info("[start] PessoaController - postPessoa");
+        PessoaResponse pessoaCriada = pessoaService.postPessoa(pessoaRequest);
+        log.info("[finish] PessoaController - postPessoa");
+        return pessoaCriada;
+    }
 }
