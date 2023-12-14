5a154ce668a7d6eebe07e6f05769a9edeb827010
unknown <zelto@hotmail.com>
2023-12-14 06:36:03 -0300

    implements method tornarPrincipal no Endereco domain

--- src/main/java/gerenciamento/com/lira/gerenciapessoas/endereco/domain/Endereco.java
+++ src/main/java/gerenciamento/com/lira/gerenciapessoas/endereco/domain/Endereco.java
@@ -3,10 +3,7 @@ package gerenciamento.com.lira.gerenciapessoas.endereco.domain;
 import gerenciamento.com.lira.gerenciapessoas.endereco.application.api.request.EnderecoRequest;
 import gerenciamento.com.lira.gerenciapessoas.pessoa.domain.Pessoa;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -19,20 +16,39 @@ public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
-    private UUID idEnderco;
+    private UUID idEndereco;
 
     private String cep;
     private String logradouro;
     private String cidade;
     private String numero;
 
+    @Setter
+    private boolean principal;
+
     @ManyToOne
     @JoinColumn(name = "pessoa_id")
     private Pessoa pessoa;
+    
+    public Endereco(EnderecoRequest enderecoRequestRequest, boolean principal) {
+        this.logradouro = enderecoRequestRequest.getLogradouro();
+        this.cidade = enderecoRequestRequest.getCidade();
+        this.numero = enderecoRequestRequest.getNumero();
+        this.principal = principal;
+    }
+
     public Endereco(EnderecoRequest enderecoRequestRequest) {
         this.logradouro = enderecoRequestRequest.getLogradouro();
         this.cidade = enderecoRequestRequest.getCidade();
         this.numero = enderecoRequestRequest.getNumero();
     }
 
+    public void tornarPrincipal() {
+        this.principal = true;
+        if (pessoa != null && pessoa.getEndereco() != null) {
+            pessoa.getEndereco().stream()
+                    .filter(endereco -> !endereco.equals(this))
+                    .forEach(endereco -> endereco.setPrincipal(false));
+        }
+    }
 }
