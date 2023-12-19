ad908705b68f02eac63ec6ac46119ac26f571461
unknown <zelto@hotmail.com>
2023-12-19 06:45:11 -0300

    implements logic to return person and adress

--- src/main/java/gerenciamento/com/lira/gerenciapessoas/pessoa/application/api/response/PessoaDetalhadoResponse.java
+++ src/main/java/gerenciamento/com/lira/gerenciapessoas/pessoa/application/api/response/PessoaDetalhadoResponse.java
@@ -1,5 +1,6 @@
 package gerenciamento.com.lira.gerenciapessoas.pessoa.application.api.response;
 
+import gerenciamento.com.lira.gerenciapessoas.endereco.application.api.response.EnderecoResponse;
 import gerenciamento.com.lira.gerenciapessoas.pessoa.domain.Pessoa;
 import gerenciamento.com.lira.gerenciapessoas.pessoa.domain.Sexo;
 import jakarta.persistence.EnumType;
@@ -9,6 +10,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -18,11 +20,15 @@ public class PessoaDetalhadoResponse {
     private String nomePessoa;
     private Sexo sexo;
     private String dataNascimento;
+    private String cpf;
+    private List<EnderecoResponse> enderecos;
 
     public PessoaDetalhadoResponse(Pessoa pessoa) {
         this.idPessoa = pessoa.getIdPessoa();
         this.nomePessoa = pessoa.getNomePessoa();
         this.sexo = pessoa.getSexo();
         this.dataNascimento = pessoa.getDataNascimento();
+        this.cpf = pessoa.getCpf();
+        this.enderecos = EnderecoResponse.converte(pessoa.getEndereco());
     }
 }
